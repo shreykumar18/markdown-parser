d5f6e97220fa0d5ec5869134a3975c6d0fde5efe
shreykumar18 <s9kumar@ucsd.edu>
2022-05-02 19:43:34 -0700

    Edits

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,14 +9,38 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        
+
+        String[] mdList = markdown.split("");
+        int openParenIndex = 0;
+        int closeParenIndex = 0;
+
+        for (int i = 0; i < markdown.length(); ++i) {
+            if (mdList[i].equals("(")) {
+                openParenIndex = i;
+                if (mdList[i+1].equals(")")) {
+                    toReturn.add("Missing Link");
+                }
+                else {
+                    int currentIndex = openParenIndex;
+                    Boolean found = false;
+                    while (found == false) {
+                        if (mdList[currentIndex].equals(")")) {
+                            closeParenIndex = currentIndex;
+                            found = true;
+                        }
+                        currentIndex++;
+                    }
+                    String link = "";
+                    for (int j = openParenIndex+1; j < closeParenIndex; ++j) {
+
+                        link += mdList[j];
+                    }
+
+                    toReturn.add(link);
+                }
+            }
+            
         }
         return toReturn;
     }
